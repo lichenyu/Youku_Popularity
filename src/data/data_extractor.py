a815444738cc4f21693e2800068acbe05da56bbf
babytuan <ouyangshuxin@gmail.com>
2016-02-26 16:24:04 +0800

    get vci files

--- src/data/data_extractor.py
+++ src/data/data_extractor.py
@@ -138,6 +138,28 @@ def clean_data(in_path, out_path, start_date, days_2end):
         out_fd.close()
         
         first_date = first_date + day_delta
+        
+def merge_files(in_files, out_file):
+    out_fd = open(out_file, 'w')
+    for f in in_files:
+        in_fd = open(f, 'r')
+        for line in in_fd.readlines():
+            out_fd.write(line)
+        in_fd.close()
+    out_fd.close()
+    
+def get_vci(in_file, out_file):
+    in_fd = open(in_file, 'r')
+    out_fd = open(out_file, 'w')
+    for line in in_fd.readlines():
+        fields = line.strip().split('\t', -1)
+        # vid, vc1, vc2, ..., vc30
+        out_fd.write(fields[0] + '\t' + fields[1])
+        for i in range(2, 31):
+            out_fd.write('\t' + str(int(fields[i]) - int(fields[i-1])))
+        out_fd.write('\n')
+    in_fd.close()
+    out_fd.close()
 
 if '__main__' == __name__:
     workpath = '/Users/ouyangshuxin/Documents/Youku_Popularity/Youku_Popularity/'
@@ -172,18 +194,33 @@ if '__main__' == __name__:
 #                workpath + 'data/clean_data/', 
 #                '2015-12-26', 10)
     
-    check_lost(workpath + 'data/clean_data/', 
-               workpath + 'data/check_lost2/', 
-               '2015-12-06', 19)
-    check_lost(workpath + 'data/clean_data/', 
-               workpath + 'data/check_lost2/', 
-               '2015-12-26', 10)
-    check_increase(workpath + 'data/clean_data/', 
-                   workpath + 'data/check_increase2/', 
-                   '2015-12-06', 19)
-    check_increase(workpath + 'data/clean_data/', 
-                   workpath + 'data/check_increase2/', 
-                   '2015-12-26', 10)
+#     check_lost(workpath + 'data/clean_data/', 
+#                workpath + 'data/check_lost2/', 
+#                '2015-12-06', 19)
+#     check_lost(workpath + 'data/clean_data/', 
+#                workpath + 'data/check_lost2/', 
+#                '2015-12-26', 10)
+#     check_increase(workpath + 'data/clean_data/', 
+#                    workpath + 'data/check_increase2/', 
+#                    '2015-12-06', 19)
+#     check_increase(workpath + 'data/clean_data/', 
+#                    workpath + 'data/check_increase2/', 
+#                    '2015-12-26', 10)
+
+    date_strs = ['2015-12-06', '2015-12-07', '2015-12-08', '2015-12-09', '2015-12-10', 
+                '2015-12-11', '2015-12-12', '2015-12-13', '2015-12-14', '2015-12-15', 
+                '2015-12-16', '2015-12-17', '2015-12-18', '2015-12-19', '2015-12-20', 
+                '2015-12-21', '2015-12-22', '2015-12-23', '2015-12-24', '2015-12-26', 
+                '2015-12-27', '2015-12-28', '2015-12-29', '2015-12-30', '2015-12-31', 
+                '2016-01-01', '2016-01-02', '2016-01-03', '2016-01-04']
+    
+#     for date in date_strs:
+#         get_vci(workpath + 'data/clean_data/' + date, workpath + 'data/vci_files/' + date)
+    
+    in_files = []
+    for date in date_strs:
+        in_files.append(workpath + 'data/vci_files/' + date)
+    merge_files(in_files, workpath + 'data/vci_files/vci')
     
     print('All Done!')
     
