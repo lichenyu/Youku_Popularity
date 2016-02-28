5e4d27b354c5609a6bab69bb7591eea9f854b600
lichenyu <lcy@kuanguang.local>
2016-02-28 12:08:01 +0800

    plot cat rank

---
+++ characterization/publication_perday/cat_rank_perday.R
@@ -0,0 +1,61 @@
+workpath = 'F:/Youku_Popularity/Youku_Popularity/'
+#workpath = '/Users/ouyangshuxin/Documents/Youku_Popularity/Youku_Popularity/'
+
+data = read.table(paste(workpath, 'characterization/publication_perday/cat_rank_perday', sep = ''))
+
+
+cols = rainbow(24)
+#d,l,u,r
+par(mar=c(5, 4, 1, 2) + 0.1)
+plot(data$V1, type = 'l', 
+     ylim = c (0, 24), axes = FALSE, 
+     main = '', sub = '', xlab = 'Publication Date', ylab = 'Category Index', 
+     col = cols[24]
+     )
+for(i in seq(2, 24)){
+  var = paste()
+  lines(eval(parse(text = paste('data$V', i, sep = '')))
+        , type = 'l', col = cols[24 - i + 1])
+}
+axis(side = 1, at = c(1, 29), labels = c('2015-12-06', '2016-01-04'))
+axis(side = 2, at = seq(0, 24, 4), labels = seq(25, 1, -4), las = 2)
+axis(side = 4, at = seq(0, 24, 4), labels = seq(25, 1, -4), las = 2, lab = 'Rank')
+
+d = rbind(c, u)
+
+pdf(paste(workpath, "characterization/publication_perday/video_uploader_perday.pdf", sep = ''), 
+    width = 5, height = 4)
+
+
+
+
+
+
+for(i in seq(2, 24)){
+  print(i)
+  var = paste('V', i, sep = '')
+  print(var)
+  lines(data$var, type = 'l', col = cols[24 - i])
+}
+
+
+
+
+
+
+
+
+legend("topright", inset = c(0, 0), 
+       pch = c(15, 0), col = c("grey", "grey"), 
+       legend = c("Video Count","Uploader Count"), bg = "white", cex = 0.8)
+
+dev.off()
+
+# #d,l,u,r
+# par(mar=c(5, 4, 1, 2) + 0.1)
+# barpos = barplot(c, 
+#                  ylim = c(0, 30000), 
+#                  main = '', sub = '', xlab = 'Publication Date', ylab = 'Publication Count', 
+#                  space = 2, col = 'grey', border = 'grey')
+# axis(side = 1, at = c(barpos[1], barpos[29]), labels = c('2015-12-06', '2016-01-04'))
+
--- src/characterization/category_perday.py
+++ src/characterization/category_perday.py
@@ -18,15 +18,33 @@ def get_transmap(in_file, out_file):
     
     out_fd = open(out_file, 'w')
     for k in cat_count_map.keys():
-        out_fd.write(k.encode('utf-8') + '\t' + str(m[k]) + '\n')
+        out_fd.write(k.encode('utf-8') + '\t' + str(cat_count_map[k]) + '\n')
     out_fd.close()
+    return cat_count_map
 
 # get (transformed) rank of each cat
+def get_cat_rank_perday(in_files, out_file, transmap):
+    out_fd = open(out_file, 'w')
+    for f in in_files:
+        # get count of each cat
+        in_fd = codecs.open(f, 'r', 'utf-8')
+        cat_count_map = {}
+        for line in in_fd.readlines():
+            fields = line.strip().split('\t', -1)
+            # cat, video_count_percat, collected_count_percat
+            cat_count_map[transmap[fields[0]]] = int(fields[1])
+        in_fd.close()
+        # rank
+        sorted_map = sorted(cat_count_map.items(), lambda i1, i2: cmp(i1[1], i2[1]))
+        for i in range(0, len(sorted_map)):
+            cat_count_map[sorted_map[i][0]] = i + 1
+        # output
+        for i in range(0, len(transmap)):
+            out_fd.write(str(cat_count_map[i + 1]) + '\t')
+        out_fd.write('\n')
+    out_fd.close()
 
-
-
-
-def get_top5cat_pct(infile):
+def get_cat_pct(infile):
     in_fd = codecs.open(infile, 'r', 'utf-8')
     cat_count_map = {}
     total_count = 0
@@ -37,22 +55,17 @@ def get_top5cat_pct(infile):
         total_count = total_count + int(fields[1])
     in_fd.close()
     sorted_map = sorted(cat_count_map.items(), lambda i1, i2: cmp(i1[1], i2[1]), reverse = True)
-    top5cat_pct = []
-    for i in range(0, 24):
-        top5cat_pct.append([sorted_map[i][0], sorted_map[i][1] * 1. / total_count])
-    return top5cat_pct
+    cat_pct = []
+    for i in range(0, len(cat_count_map)):
+        cat_pct.append([sorted_map[i][0], sorted_map[i][1] * 1. / total_count])
+    return cat_pct
 
-def get_top5cat_pct_perday(in_files, out_file):
-    out_fd = open(out_file, 'w')
-    for f in in_files:
-        for s in get_top5cat_pct(f):
-            out_fd.write(s[0].encode('utf-8') + '\t' + str(s[1]) + '\n')
-        out_fd.write('\n')
-    out_fd.close()
 
 if '__main__' == __name__:
-    datapath = '/Users/ouyangshuxin/Documents/Youku_Popularity/'
-    workpath = '/Users/ouyangshuxin/Documents/Youku_Popularity/Youku_Popularity/'
+#     datapath = '/Users/ouyangshuxin/Documents/Youku_Popularity/'
+#     workpath = '/Users/ouyangshuxin/Documents/Youku_Popularity/Youku_Popularity/'
+    datapath = 'F:/Youku_Popularity/'
+    workpath = 'F:/Youku_Popularity/Youku_Popularity/'
     
     date_strs = ['2015-12-06', '2015-12-07', '2015-12-08', '2015-12-09', '2015-12-10', 
                 '2015-12-11', '2015-12-12', '2015-12-13', '2015-12-14', '2015-12-15', 
@@ -61,14 +74,14 @@ if '__main__' == __name__:
                 '2015-12-27', '2015-12-28', '2015-12-29', '2015-12-30', '2015-12-31', 
                 '2016-01-01', '2016-01-02', '2016-01-03', '2016-01-04']
    
-    m = get_transmap(datapath + 'rawdata/total_count/2015-12-06')
-    for k in m.keys():
-        print(k.encode('utf-8') + '\t' + str(m[k]))
-    
+    transmap = get_transmap(datapath + 'rawdata/total_count/' + date_strs[0], 
+                            workpath + 'characterization/publication_perday/category_nametrans')
     
     in_files = []
     for date in date_strs:
         in_files.append(datapath + 'rawdata/total_count/' + date)
-    get_top5cat_pct_perday(in_files, workpath + 'characterization/publication_perday/top5cat_pct_perday')
+    get_cat_rank_perday(in_files, 
+                        workpath + 'characterization/publication_perday/cat_rank_perday', 
+                        transmap)
     
     print('All Done!')
