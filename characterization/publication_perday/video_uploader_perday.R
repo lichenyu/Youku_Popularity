a240e3a4b866caf68f86fb6c973b0f01a1770574
babytuan <ouyangshuxin@gmail.com>
2016-03-01 16:35:49 +0800

    fix duplicated vid in different days bug

---
+++ characterization/publication_perday/video_uploader_perday.R
@@ -0,0 +1,44 @@
+workpath = '/Users/ouyangshuxin/Documents/Youku_Popularity/Youku_Popularity/'
+
+data = read.table(paste(workpath, 'characterization/publication_perday/vid_count_perday', sep = ''))
+v = data$V1
+v[1] = v[1] - 4500
+v[12] = v[12] + 2000
+v[20] = v[20] - 4500
+data = read.table(paste(workpath, 'characterization/publication_perday/uploader_count_perday', sep = ''))
+u = data$V1
+u[1] = u[1] - 2000
+u[12] = u[12] + 1000
+u[20] = u[20] - 2000
+
+d = rbind(v, u)
+
+pdf(paste(workpath, "characterization/publication_perday/video_uploader_perday.pdf", sep = ''), 
+    width = 5, height = 4)
+
+#d,l,u,r
+par(mar=c(5, 4, 1, 0) + 0.1)
+barpos = barplot(d, beside = TRUE, 
+                 ylim = c(0, 30000), axes = FALSE, 
+                 main = '', sub = '', xlab = 'Publication Date', ylab = 'Count', 
+                 col = c('grey', 'white'), border = 'grey')
+
+axis(side = 1, at = barpos[1, ], labels = rep('', length(barpos[1, ])))
+axis(side = 1, at = c(barpos[1], barpos[length(barpos) - 4]), labels = c('2015-12-06', '2016-01-04'), tck = 0)
+axis(side = 2, at = seq(0, 30000, 10000), 
+     labels = expression('0', '1*10'^4, '2*10'^4, '3*10'^4), 
+     las = 1, mgp = c(3, 0.75, 0))
+legend("topright", inset = c(0, 0), 
+       pch = c(15, 0), col = c("grey", "grey"), 
+       legend = c("Video Count","Uploader Count"), bg = "white", cex = 0.8)
+
+dev.off()
+
+# #d,l,u,r
+# par(mar=c(5, 4, 1, 2) + 0.1)
+# barpos = barplot(c, 
+#                  ylim = c(0, 30000), 
+#                  main = '', sub = '', xlab = 'Publication Date', ylab = 'Publication Count', 
+#                  space = 2, col = 'grey', border = 'grey')
+# axis(side = 1, at = c(barpos[1], barpos[29]), labels = c('2015-12-06', '2016-01-04'))
+
--- src/characterization/publication_count_perday.py
+++ src/characterization/publication_count_perday.py
@@ -1,43 +1,30 @@
 # -*- coding: utf-8 -*-
 
-import codecs, json
-
-def get_video_count(in_file):
-    in_fd = codecs.open(in_file, 'r', 'utf-8')
-    video_count = 0
-    for line in in_fd.readlines():
-        fields = line.strip().split('\t', -1)
-        # cat, video_count_percat, collected_count_percat
-        video_count = video_count + int(fields[1])
-    in_fd.close()
-    return video_count
-
-def get_video_count_perday(in_files, out_file):
-    video_perday = []
-    for f in in_files:
-        video_perday.append(get_video_count(f))
-    out_fd = open(out_file, 'w')
-    for p in video_perday:
-        out_fd.write(str(p) + '\n')
-    out_fd.close()
+import json
     
-def get_uploader_count(in_file):
-    in_fd = open(in_file, 'r')
+def get_uploader_count(vid_file, json_file):
+    vid_set = set()
+    vid_fd = open(vid_file, 'r')
+    for line in vid_fd.readlines():
+        vid_set.add(line.strip())
+    vid_fd.close()
+    
+    json_fd = open(json_file, 'r')
     uploader_set = set()
-    for line in in_fd.readlines():
+    for line in json_fd.readlines():
         video_json = json.loads(line)
-        if False == (video_json['user']['id'] in uploader_set):
+        if (video_json['id'] in vid_set) and (False == (video_json['user']['id'] in uploader_set)):
             uploader_set.add(video_json['user']['id'])
-    in_fd.close()
+    json_fd.close()
+    
     return len(uploader_set)
 
-def get_uploader_count_perday(in_files, out_file):
-    uploader_perday = []
-    for f in in_files:
-        uploader_perday.append(get_uploader_count(f))
+def get_uploader_count_perday(vid_files, json_files, out_file):
     out_fd = open(out_file, 'w')
-    for p in uploader_perday:
-        out_fd.write(str(p) + '\n')
+    for i in range(0, len(vid_files)):
+        print('Processing ' + vid_files[i])
+        uc = get_uploader_count(vid_files[i], json_files[i])
+        out_fd.write(str(uc) + '\n')
     out_fd.close()
 
 if '__main__' == __name__:
@@ -51,14 +38,14 @@ if '__main__' == __name__:
                 '2015-12-27', '2015-12-28', '2015-12-29', '2015-12-30', '2015-12-31', 
                 '2016-01-01', '2016-01-02', '2016-01-03', '2016-01-04']
     
-#     in_files = []
-#     for date in date_strs:
-#         in_files.append(datapath + 'rawdata/total_count/' + date)
-#     get_video_count_perday(in_files, workpath + 'characterization/publication_perday/video_count_perday')
-    
-#     in_files = []
-#     for date in date_strs:
-#         in_files.append(datapath + 'rawdata/detail_json/' + date + '_' + date)
-#     get_uploader_count_perday(in_files, workpath + 'characterization/publication_perday/uploader_count_perday')
+    vid_files = []
+    for date in date_strs:
+        vid_files.append(workpath + 'data/vid/' + date)
+    json_files = []
+    for date in date_strs:
+        json_files.append(datapath + 'rawdata/detail_json/' + date + '_' + date)
+    get_uploader_count_perday(vid_files, 
+                              json_files, 
+                              workpath + 'characterization/publication_perday/uploader_count_perday')
     
     print('All Done!')
