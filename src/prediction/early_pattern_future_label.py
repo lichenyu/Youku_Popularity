da9759ea1a78caaeec3df05ab13d6f9639960302
babytuan <ouyangshuxin@gmail.com>
2016-03-10 10:55:42 +0800

    get future label

--- src/prediction/early_pattern_future_label.py
+++ src/prediction/early_pattern_future_label.py
@@ -68,56 +68,48 @@ def count_results(in_file, out_file, result_idx):
     for v in sorted_list:
         out_fd.write(v[0] + '\t' + str(v[1]) + '\t%.02f' % (v[1] * 100. / total) + '%\n')
     out_fd.close()
-
-def add_futureburst_2ifiles(vci_file, scale_val, scale_inc, pattern_file, ear1_frac, ear0_frac, label_file):
-    # whether early burst exists -> early burst set
-    # get early pattern
-    earlyburst_set = set()
-    vci_fd = open(vci_file, 'r')
-    pattern_fd = open(pattern_file, 'w')
-
-        
-        
     
-#     out_fd = open(out_file, 'w')
-#     vci_fd = open(vci_file, 'r')
-#     for line in vci_fd.readlines():
-#         fields = line.strip().split('\t', -1)
-#         # vid, vci1, vci2, ..., vci30
-#         # check early 0 future 0 with ear0_frac
-#         if False == (fields[0] in earlyburst_set) and False == (fields[0] in futureburst_set):
-#             vci_list = []
-#             for i in range(1, 31):
-#                 vci_list.append(int(fields[i]))
-#             if 0 == sum(vci_list):
-#                 continue
-#             if ear0_frac <= (1. * sum(vci_list[7 : 30]) / sum(vci_list)):
-#                 out_fd.write(line.strip() + '\tTrue\n')
-#             else:
-#                 pass#out_fd.write(line.strip() + '\tFalse\n')
-#         # check early 1 future 0 with ear1_frac
-#         elif True == (fields[0] in earlyburst_set) and False == (fields[0] in futureburst_set):
-#             vci_list = []
-#             for i in range(1, 31):
-#                 vci_list.append(int(fields[i]))
-#             if 0 == sum(vci_list):
-#                 continue
-#             if ear1_frac <= (1. * sum(vci_list[7 : 30]) / sum(vci_list)):
-#                 out_fd.write(line.strip() + '\tTrue\n')
-#             else:
-#                 pass#out_fd.write(line.strip() + '\tFalse\n')
-#         # for early 0 future 1
-#         elif False == (fields[0] in earlyburst_set) and True == (fields[0] in futureburst_set):
-#             pass#out_fd.write(line.strip() + '\tTrue\n')
-#         # for early 1 future 1
-#         elif True == (fields[0] in earlyburst_set) and True == (fields[0] in futureburst_set):
-#             pass#out_fd.write(line.strip() + '\tTrue\n')
-#         else:
-#             print('Impossible: ' + fields[0])
-#     vci_fd.close()    
-#     out_fd.close()
-
-
+def get_future_label(earlypattern_file, vci_file, ear1_frac, ear0_frac, out_file):
+    earlyburst_set = set()
+    earlypattern_fd = open(earlypattern_file, 'r')
+    for line in earlypattern_fd.readlines():
+        fields = line.strip().split('\t', -1)
+        # vid, early_pattern
+        for i in range(0, len(fields[1])):
+            if '1' == fields[1][i]:
+                earlyburst_set.add(fields[0])
+                break
+    earlypattern_fd.close()
+    
+    vci_fd = open(vci_file, 'r')
+    out_fd = open(out_file, 'w')
+    for line in vci_fd.readlines():
+        fields = line.strip().split('\t', -1)
+        # vid, vci1, vci2, ..., vci30
+        vci_list = []
+        for i in range(1, 1 + 30):
+            vci_list.append(int(fields[i]))
+        
+        # 1 in early pattern
+        if fields[0] in earlyburst_set:
+            if 0 == sum(vci_list):
+                continue
+            if ear1_frac <= (1. * sum(vci_list[7 : 30]) / sum(vci_list)):
+                out_fd.write(line.strip() + '\tTrue\n')
+            else:
+                out_fd.write(line.strip() + '\tFalse\n')
+        # no 1 in early pattern
+        else:
+            if 0 == sum(vci_list):
+                continue
+            if ear0_frac <= (1. * sum(vci_list[7 : 30]) / sum(vci_list)):
+                out_fd.write(line.strip() + '\tTrue\n')
+            else:
+                out_fd.write(line.strip() + '\tFalse\n')
+    vci_fd.close()
+    out_fd.close()
+    
+    
 
 if '__main__' == __name__:
     workpath = '/Users/ouyangshuxin/Documents/Youku_Popularity/Youku_Popularity/'
@@ -129,5 +121,10 @@ if '__main__' == __name__:
 #     count_results(workpath + 'prediction/early_pattern_future_burst/early_pattern', 
 #                   workpath + 'prediction/early_pattern_future_burst/early_pattern_count', 
 #                   1)
+
+    get_future_label(workpath + 'prediction/early_pattern_future_burst/early_pattern', 
+                     workpath + 'data/vci_files/vci', 
+                     0.8 * 23 / 30, 1.2 * 23 / 30, 
+                     workpath + 'prediction/early_pattern_future_burst/future_label')
 
     print('All Done!')
