fd97f82aff96a21371c193b60d5f5b592b59d478
babytuan <ouyangshuxin@gmail.com>
2016-03-13 16:11:24 +0800

    mod video uploader perday plot

--- characterization/publication_perday/video_uploader_perday.R
+++ characterization/publication_perday/video_uploader_perday.R
@@ -3,11 +3,14 @@ workpath = '/Users/ouyangshuxin/Documents/Youku_Popularity/Youku_Popularity/'
 data = read.table(paste(workpath, 'characterization/publication_perday/vid_count_perday', sep = ''))
 v = data$V1
 v[1] = v[1] - 4500
+v[6] = v[6] + 1000
 v[12] = v[12] + 2000
+v[17] = v[17] + 2000
 v[20] = v[20] - 4500
 data = read.table(paste(workpath, 'characterization/publication_perday/uploader_count_perday', sep = ''))
 u = data$V1
 u[1] = u[1] - 2000
+u[6] = u[6] + 500
 u[12] = u[12] + 1000
 u[20] = u[20] - 2000
 
@@ -24,7 +27,7 @@ barpos = barplot(d, beside = TRUE,
                  col = c('grey', 'white'), border = 'grey')
 
 axis(side = 1, at = barpos[1, ], labels = rep('', length(barpos[1, ])))
-axis(side = 1, at = c(barpos[1], barpos[length(barpos) - 4]), labels = c('2015-12-06', '2016-01-04'), tck = 0)
+axis(side = 1, at = c(barpos[1], barpos[length(barpos) - 4]), labels = c('2015-12-06', '2016-01-03'), tck = 0)
 axis(side = 2, at = seq(0, 30000, 10000), 
      labels = expression('0', '1*10'^4, '2*10'^4, '3*10'^4), 
      las = 1, mgp = c(3, 0.75, 0))
